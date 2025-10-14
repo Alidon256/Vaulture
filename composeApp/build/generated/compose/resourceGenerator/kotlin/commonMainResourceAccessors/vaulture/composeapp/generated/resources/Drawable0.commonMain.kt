f932f77533c0e7c9d37aa40ca28925dd9ff48b8f
ANHONY <mugumyaali@outlook.com>
2025-10-14 16:17:52 +0300

    working on the theme

--- composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/vaulture/composeapp/generated/resources/Drawable0.commonMain.kt
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/vaulture/composeapp/generated/resources/Drawable0.commonMain.kt
@@ -17,14 +17,147 @@ internal val Res.drawable.authentication: DrawableResource by lazy {
       ))
     }
 
+internal val Res.drawable.bg_one: DrawableResource by lazy {
+      DrawableResource("drawable:bg_one", setOf(
+        ResourceItem(setOf(), "${MD}drawable/bg_one.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.bg_two: DrawableResource by lazy {
+      DrawableResource("drawable:bg_two", setOf(
+        ResourceItem(setOf(), "${MD}drawable/bg_two.jpg", -1, -1),
+      ))
+    }
+
 internal val Res.drawable.compose_multiplatform: DrawableResource by lazy {
       DrawableResource("drawable:compose_multiplatform", setOf(
         ResourceItem(setOf(), "${MD}drawable/compose-multiplatform.xml", -1, -1),
       ))
     }
 
+internal val Res.drawable.egypt: DrawableResource by lazy {
+      DrawableResource("drawable:egypt", setOf(
+        ResourceItem(setOf(), "${MD}drawable/egypt.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.greece: DrawableResource by lazy {
+      DrawableResource("drawable:greece", setOf(
+        ResourceItem(setOf(), "${MD}drawable/greece.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.ic_hotel1: DrawableResource by lazy {
+      DrawableResource("drawable:ic_hotel1", setOf(
+        ResourceItem(setOf(), "${MD}drawable/ic_hotel1.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.ic_hotel2: DrawableResource by lazy {
+      DrawableResource("drawable:ic_hotel2", setOf(
+        ResourceItem(setOf(), "${MD}drawable/ic_hotel2.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.ic_hotels: DrawableResource by lazy {
+      DrawableResource("drawable:ic_hotels", setOf(
+        ResourceItem(setOf(), "${MD}drawable/ic_hotels.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.ic_img1: DrawableResource by lazy {
+      DrawableResource("drawable:ic_img1", setOf(
+        ResourceItem(setOf(), "${MD}drawable/ic_img1.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.kenya: DrawableResource by lazy {
+      DrawableResource("drawable:kenya", setOf(
+        ResourceItem(setOf(), "${MD}drawable/kenya.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.logo: DrawableResource by lazy {
+      DrawableResource("drawable:logo", setOf(
+        ResourceItem(setOf(), "${MD}drawable/logo.png", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.maps: DrawableResource by lazy {
+      DrawableResource("drawable:maps", setOf(
+        ResourceItem(setOf(), "${MD}drawable/maps.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.nigeria: DrawableResource by lazy {
+      DrawableResource("drawable:nigeria", setOf(
+        ResourceItem(setOf(), "${MD}drawable/nigeria.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.profile: DrawableResource by lazy {
+      DrawableResource("drawable:profile", setOf(
+        ResourceItem(setOf(), "${MD}drawable/profile.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.spain: DrawableResource by lazy {
+      DrawableResource("drawable:spain", setOf(
+        ResourceItem(setOf(), "${MD}drawable/spain.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.travel: DrawableResource by lazy {
+      DrawableResource("drawable:travel", setOf(
+        ResourceItem(setOf(), "${MD}drawable/travel.webp", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.travel2: DrawableResource by lazy {
+      DrawableResource("drawable:travel2", setOf(
+        ResourceItem(setOf(), "${MD}drawable/travel2.webp", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.uganda: DrawableResource by lazy {
+      DrawableResource("drawable:uganda", setOf(
+        ResourceItem(setOf(), "${MD}drawable/uganda.jpg", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.val_1: DrawableResource by lazy {
+      DrawableResource("drawable:val_1", setOf(
+        ResourceItem(setOf(), "${MD}drawable/val_1.png", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.val_2: DrawableResource by lazy {
+      DrawableResource("drawable:val_2", setOf(
+        ResourceItem(setOf(), "${MD}drawable/val_2.png", -1, -1),
+      ))
+    }
+
 @InternalResourceApi
 internal fun _collectCommonMainDrawable0Resources(map: MutableMap<String, DrawableResource>) {
   map.put("authentication", Res.drawable.authentication)
+  map.put("bg_one", Res.drawable.bg_one)
+  map.put("bg_two", Res.drawable.bg_two)
   map.put("compose_multiplatform", Res.drawable.compose_multiplatform)
+  map.put("egypt", Res.drawable.egypt)
+  map.put("greece", Res.drawable.greece)
+  map.put("ic_hotel1", Res.drawable.ic_hotel1)
+  map.put("ic_hotel2", Res.drawable.ic_hotel2)
+  map.put("ic_hotels", Res.drawable.ic_hotels)
+  map.put("ic_img1", Res.drawable.ic_img1)
+  map.put("kenya", Res.drawable.kenya)
+  map.put("logo", Res.drawable.logo)
+  map.put("maps", Res.drawable.maps)
+  map.put("nigeria", Res.drawable.nigeria)
+  map.put("profile", Res.drawable.profile)
+  map.put("spain", Res.drawable.spain)
+  map.put("travel", Res.drawable.travel)
+  map.put("travel2", Res.drawable.travel2)
+  map.put("uganda", Res.drawable.uganda)
+  map.put("val_1", Res.drawable.val_1)
+  map.put("val_2", Res.drawable.val_2)
 }
